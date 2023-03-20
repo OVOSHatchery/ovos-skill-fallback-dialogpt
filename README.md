c57ca45182b454a8d3d4959b4642284dde3fc963
jarbasai <jarbasai@mailfence.com>
2023-03-20 09:36:24 +0000

    :tada:

---
+++ README.md
@@ -0,0 +1,23 @@
+# DialoGPT Fallback Skill
+
+When in doubt, ask DialoGPT, powered by [DialoGPT Persona](https://github.com/OpenVoiceOS/ovos-solver-plugin-dialogpt)
+
+Offline alternative to ChatGPT skill  (GPT 2 based)
+
+## About 
+
+While this can not compete with [ChatGPT Persona](https://github.com/OpenVoiceOS/ovos-solver-plugin-openai-persona) it is an offline alternative, you can use any model from huggingface to have different personas
+
+Find models [here](https://huggingface.co/models?pipeline_tag=conversational&search=dialogpt)
+
+## Configuration
+
+Under skill settings you can tweak some parameters for DialoGPT
+
+- `model` - model to use, default `"microsoft/DialoGPT-medium"`
+
+## Examples 
+
+* "What is best in life?"
+* "Do you like dogs"
+* "Does God exist?"
---
+++ __init__.py
@@ -0,0 +1,35 @@
+from ovos_solver_dialogpt import DialoGPTSolver
+from ovos_utils import classproperty
+from ovos_utils.process_utils import RuntimeRequirements
+from ovos_workshop.skills.fallback import FallbackSkill
+
+
+class DialoGPTSkill(FallbackSkill):
+
+    @classproperty
+    def runtime_requirements(self):
+        return RuntimeRequirements(internet_before_load=False,
+                                   network_before_load=False,
+                                   gui_before_load=False,
+                                   requires_internet=False,
+                                   requires_network=False,
+                                   requires_gui=False,
+                                   no_internet_fallback=True,
+                                   no_network_fallback=True,
+                                   no_gui_fallback=True)
+
+    def initialize(self):
+        self._chat = DialoGPTSolver(config=self.settings)
+        self.register_fallback(self.ask_dialogpt, 88)
+
+    def ask_dialogpt(self, message):
+        utterance = message.data['utterance']
+        answer = self._chat.get_spoken_answer(utterance, context=message.context)
+        if not answer:
+            return False
+        self.speak(answer)
+        return True
+
+
+def create_skill():
+    return DialoGPTSkill()
